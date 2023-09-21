926d620add7bf8389a91d10be70b8590f2429311
Joel Norrman <joel-norrman@hotmail.com>
2023-09-21 13:53:40 +0200

    final merge

--- src/com/Joel/gitstuff/Main.java
+++ src/com/Joel/gitstuff/Main.java
@@ -3,5 +3,8 @@ package com.Joel.gitstuff;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello git");
+        //Merged "hello git this is master" and "hello git this is newBranch" result from just default-
+        //accepting merge is just "hello git" maybe from earlier version of code? or maybe based on-
+        //similarities? if from similarities where did "this is" go?
     }
 }
